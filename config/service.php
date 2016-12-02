88cb62ff633065224c5a5b66583bfc08eb860d68
devosc <devosc@gmail.com>
2016-12-02 14:54:04 -0600

    move static service context config to mvc5 service config

--- config/service.php
+++ config/service.php
@@ -3,12 +3,8 @@
  *
  */
 
-use Mvc5\Plugin\Invoke;
-use Mvc5\Plugin\Link;
 use View5\Compiler\ViewTemplate;
 
 return [
-    'service\context' => new Invoke(Mvc5\Service\Context::class, ['service' => new Link]),
-    'view5\template' => [ViewTemplate::class, ['import' => ['arc5']]],
-    'web\context' => [Mvc5\Web\Context::class, new Link],
+    'view5\template' => [ViewTemplate::class, ['import' => ['arc5']]]
 ];
