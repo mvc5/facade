65bf9998614ebfaa6541abc521f6c13f4e6a7a09
devosc <devosc@gmail.com>
2017-09-28 21:24:23 -0500

    separate compiler components

--- config/service.php
+++ config/service.php
@@ -3,8 +3,15 @@
  *
  */
 
+use View5\Compiler\Compile\Comments;
+use View5\Compiler\Compile\Echos;
+use View5\Compiler\Compile\Extension;
+use View5\Compiler\Compile\Statements;
 use View5\Compiler\ViewTemplate;
 
 return [
-    'view5\template' => [ViewTemplate::class, ['import' => ['arc5']]]
+    'view5\template' => [ViewTemplate::class, [
+        'compiler' => [new Extension, new Statements, new Comments, new Echos],
+        'import' => ['arc5']
+    ]]
 ];
