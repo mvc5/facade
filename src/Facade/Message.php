295a13324e2feaf334ef6c404a155a4e98ab7cac
devosc <devosc@gmail.com>
2018-03-27 17:41:00 -0500

    null string name (defaults to #)

--- src/Facade/Message.php
+++ src/Facade/Message.php
@@ -7,6 +7,7 @@ namespace Arc5\Facade;
 
 use Mvc5\Arg;
 use Mvc5\Service\Facade;
+use Mvc5\Session\SessionMessages;
 
 trait Message
 {
@@ -17,53 +18,53 @@ trait Message
 
     /**
      * @param array|string $message
-     * @param string $name
+     * @param string|null $name
      */
-    static function danger($message, string $name = Arg::INDEX)
+    static function danger($message, string $name = null) : void
     {
         static::messages()->danger($message, $name);
     }
 
     /**
      * @param array|string $message
-     * @param string $name
+     * @param string|null $name
      */
-    static function info($message, string $name = Arg::INDEX)
+    static function info($message, string $name = null) : void
     {
         static::messages()->info($message, $name);
     }
 
     /**
-     * @param $name
+     * @param string|null $name
      * @return array|null
      */
-    static function message(string $name = Arg::INDEX)
+    static function message(string $name = null)
     {
         return static::messages()->message($name);
     }
 
     /**
-     * @return \Mvc5\Session\SessionMessages
+     * @return SessionMessages
      */
-    static function messages()
+    static function messages() : SessionMessages
     {
         return static::plugin(Arg::SESSION_MESSAGES);
     }
 
     /**
      * @param array|string $message
-     * @param string $name
+     * @param string|null $name
      */
-    static function success($message, string $name = Arg::INDEX)
+    static function success($message, string $name = null) : void
     {
         static::messages()->success($message, $name);
     }
 
     /**
      * @param array|string $message
-     * @param string $name
+     * @param string|null $name
      */
-    static function warning($message, string $name = Arg::INDEX)
+    static function warning($message, string $name = null) : void
     {
         static::messages()->warning($message, $name);
     }
